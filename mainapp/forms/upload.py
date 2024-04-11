c167f714b9675154f8d53cbe27f666d2780b3cad
Antonio <antoniotropiano@gmail.com>
2024-04-11 18:35:11 +0200

    add the mulit file upload

--- mainapp/forms/upload.py
+++ mainapp/forms/upload.py
@@ -1,18 +1,36 @@
 from django import forms
 from mainapp.models.upload import Document
+from django.forms import ClearableFileInput
 from django.core.validators import FileExtensionValidator
 
 
-class DocumentForm(forms.ModelForm):
+class MultipleFileInput(forms.ClearableFileInput):
+    allow_multiple_selected = True
+
+
+class DocumentForm(forms.FileField):
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("widget", MultipleFileInput())
+        super().__init__(*args, **kwargs)
+
     class Meta:
         model = Document
         fields = ("document",)
 
-    def clean(self):
-        cleaned_data = super(DocumentForm, self).clean()
-        file = cleaned_data.get("document")
-        file_name = file.name
-        if not "csv" in file_name:
-            print("Non csv file")
-            print(file_name)
-            raise forms.ValidationError("Please upload a csv file")
+    def clean(self, data, initial=None):
+        single_file_clean = super(DocumentForm, self).clean
+        if isinstance(data, (list, tuple)):
+            result = [single_file_clean(d, initial) for d in data]
+            csv_val = ["csv" in r.name for r in result]
+            if not all(csv_val):
+                raise forms.ValidationError("Please upload only csv files")
+        else:
+            result = [single_file_clean(data, initial)]
+            csv_file = "csv" in result.name
+            if not csv_file:
+                raise forms.ValidationError("Please upload only csv files")
+        return result
+
+
+class FileFieldForm(forms.Form):
+    file_field = DocumentForm()
--- mainapp/views/upload.py
+++ mainapp/views/upload.py
@@ -1,10 +1,11 @@
-from mainapp.forms.upload import DocumentForm
+from mainapp.forms.upload import DocumentForm, FileFieldForm
 from mainapp.tasks.analyze_user_tweets import score, write_db
 from django.shortcuts import render
 from django.shortcuts import redirect
 import pandas as pd
 from django.conf import settings
 from usermodel.models import User
+from mainapp.models.upload import Document
 
 
 def model_form_upload(request):
@@ -14,17 +15,24 @@ def model_form_upload(request):
         return redirect("login")
 
     if request.method == "POST":
-        form = DocumentForm(request.POST, request.FILES)
+        form = FileFieldForm(request.POST, request.FILES)
+        files = request.FILES.getlist("file_field")
+        # files = form.cleaned_data["file_field"]
+        file_insts = []
         if form.is_valid():
-            file_obj = form.save(commit=False)
-            file_obj.save()
+            for file in files:
+                file_instance = Document(document=file)
+                file_instance.save()
+                file_insts.append(file_instance.document.url)
+                # file_obj = form.save(commit=False)
+                # file_obj.save()
             #  print(file_obj.document.url)
             # execute task to populate DB
-            init_db(request, file_obj.document)
+            init_db(request, file_insts)
             return redirect("success")
 
     else:
-        form = DocumentForm()
+        form = FileFieldForm()
 
     return render(request, "mainapp/upload.html", {"form": form})
 
@@ -34,14 +42,21 @@ def success(request):
     return render(request, "mainapp/upload_success.html")
 
 
-def init_db(request, file_path):
+def init_db(request, files):
 
     # print(os.environ["MEDIA_ROOT"])
     # print(file_path)
-    tweet_file = f"{settings.MEDIA_ROOT}/{file_path}"
-    # user_name = tweet_file.split("/")[-1].split("_")[3]
-    user_name = request.user.username
-    df_full = pd.read_csv(tweet_file)
+    df_list = []
+    for file in files:
+        file_path = "/".join(file.split("/")[2:])
+        tweet_file = f"{settings.MEDIA_ROOT}/{file_path}"
+        # user_name = tweet_file.split("/")[-1].split("_")[3]
+        user_name = request.user.username
+        df_list.append(pd.read_csv(tweet_file))
+    # concatenate all dataframes
+    df_full = pd.concat(df_list)
+    # drop the duplicates (just in case)
+    df_full = df_full.drop_duplicates(subset=["Tweet permalink"])
     # apply the function that calculates the score
     df_full["score"] = df_full.apply(lambda x: score(x)[0], axis=1)
     df_full["norm_score"] = df_full.apply(lambda x: score(x)[1], axis=1)
