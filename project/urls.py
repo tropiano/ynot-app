23040dc468739f349969abc4880ae582c69adf89
Antonio <antoniotropiano@gmail.com>
2025-10-29 22:22:25 +0300

    add the new requirements

--- project/urls.py
+++ project/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from mainapp import views
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import RedirectView
 
 
 urlpatterns = [
